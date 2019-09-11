07dfc8df4aaabab64da341f6db8479a3034773ca
safronman <safronmanbox@gmail.com>
2019-09-11 20:11:41 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>1</h1>
+      <h2>2</h2>
     </div>
   );
 }
